8e8cf9544be0e757d73e9e0ff21fd41e83b9e679
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-01-06 17:09:59 +0100

    created new class Calculater

---
+++ src/main/java/first/project/App.java
@@ -0,0 +1,24 @@
+package first.project;
+
+import first.project.calculator.Calculator;
+
+import java.util.Scanner;
+
+public class App {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        char choice;
+        do{
+            int operand1 = Calculator.getInt();
+            int operand2 = Calculator.getInt();
+            char operation = Calculator.getOperation();
+            int result = Calculator.calculating(operand1, operand2, operation);
+            System.out.println("Result of the operation: " + operand1 +
+                    " " + operation + " " + operand2 + " = " + result);
+            System.out.println("Do you want to continue?\n" +
+                    "Enter Y for yes or N for no : ");
+            choice = scanner.next().charAt(0);
+        }while ((choice == 'y') || (choice == 'Y'));
+    }
+}
---
+++ src/main/java/first/project/calculator/Calculator.java
@@ -0,0 +1,55 @@
+package first.project.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static int getInt(){
+        System.out.println("Enter an integer: ");
+        int operand;
+        if(scanner.hasNextInt()){
+            operand = scanner.nextInt();
+        }else {
+            System.out.println("You made a mistake when entering a number. Try again.");
+            scanner.next();
+            operand = getInt();
+        }
+        return operand;
+    }
+    public static char getOperation(){
+        System.out.println("Enter operation (+, -, *, /): ");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        }else {
+            System.out.println("You made an error when entering an operation. Try again.");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+    public static int calculating(int operand1, int operand2, char operation){
+        int result;
+        switch (operation){
+            case '+':
+                result = operand1 + operand2;
+                break;
+            case '-':
+                result = operand1 - operand2;
+                break;
+            case '*':
+                result = operand1 * operand2;
+                break;
+            case '/':
+                result = operand1 / operand2;
+                break;
+            default:
+                System.out.println("The operation was not recognized. Please re-enter.");
+                result = 0;
+                break;
+        }
+        return result;
+    }
+
+}
